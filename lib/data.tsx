21b1dbf1c0c42ed5077ebf513ed76f1e157d0241
Emmanuel Excellent <68669639+Daddypastor@users.noreply.github.com>
2023-09-30 23:25:47 +0100

    Update data.tsx

--- lib/data.tsx
+++ lib/data.tsx
@@ -35,28 +35,52 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "META Frontend Developer Professional Course",
+    location: "COUSERA",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+      "From this Meta course, I learnt the from basics of frontend developer (using HTML, CSS and Javascript) to the advance frontend developer using react (framework built  by Meta)"
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    date: "2023",
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
+    title: "META Backend Developer Professional Course",
+    location: "COUSERA",
     description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+      "Also from this course, I learnt the all the necceasary things in backend starting from the basics of PYTHON and other database managements. Then dive into APIs and using DJANGO Framework to develop APIs."
+    icon: React.createElement(LuGraduationCap),
+    date: "2023",
+  },
+  {
+    title: "FullStack Developer",
+    location: "OOU Ibogun Campus",
+    description:
+      "I help group of students develop their School Library website project assigned to them. I was able to design and develop beautiful UI and help them manage the database using SQLite built in django. With the success of the amazing project, the students got excellent grade.",
     icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
+    date: "2022",
+  },
+  {
+    title: "AI Animal Recognition Project",
+    location: "OOU Ibogun Campus",
+    description:
+      "As I increase in networking with students around, a group of students also surmoned me to help them with their project which Animal Recognition AI. With my Python knowledge, using tensorflow, wi help them to train a model that could recognise if an image is an animal.",
+    icon: React.createElement(FaReact),
+    date: "2021 - present",
+  },
+  {
+    title: "Project Supervisor",
+    location: "Abeokuta ",
+    description:
+      "Also a team of students from a technical school surmoned me as a supervisor over their WebApp project. They're to develop an attendee webapp which can only function for students within their campus only. I was able to give clear insight of the project and help understand how they can succed in the project and I supervised each of the team members guiding through the best system till they are able to achieve their desired goal.,
+    icon: React.createElement(FaReact),
+    date: "2023",
   },
   {
     title: "Full-Stack Developer",
-    location: "Houston, TX",
+    location: "Lagos, Nigeria",
     description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma, MongoDB, MySQL, GitHub, Django, Python, AI Modeling, AWS, Google Cloud and Security Analyst. I'm open to full-time opportunities.",
     icon: React.createElement(FaReact),
-    date: "2021 - present",
+    date: "2023 - present",
   },
 ] as const;
 
