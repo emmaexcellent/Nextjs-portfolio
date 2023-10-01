fc3d386453febed5d456204264d487a3759e8ca0
Emmanuel Excellent <68669639+Daddypastor@users.noreply.github.com>
2023-10-01 21:27:59 +0100

    Update Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -30,7 +30,7 @@ export default function Contact() {
       }}>
       <SectionHeading>Contact Me</SectionHeading>
       <p className='text-gray-700b -mt-6 dark:text-white/80'>
-        Please contact me directly at <a className='underline' href='mailto:example@gmail.com'>example@gmail.com</a> or through this form</p>
+        Please contact me directly at <a className='underline' href='mailto:example@gmail.com'>emmaexcellent030@gmail.com</a> or through this form</p>
       <form className='mt-10 flex flex-col dark:text-black'
         action={async (formData) =>{
           const { data, error } = await sendEmail(formData);
