b5aaac1933700ddfdc356869f0a332addc335a15
Emmanuel Excellent <68669639+Daddypastor@users.noreply.github.com>
2023-10-01 23:02:36 +0100

    Update Project.tsx

--- components/Project.tsx
+++ components/Project.tsx
@@ -25,7 +25,7 @@ export default function Project({ title, description, tags, imageUrl }:
       }}
       className='group mb-3 sm:mb-8 last:mb-0'>
 
-      <section className='bg-gray-100 flex max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] sm:group-even:pl-8 hover:bg-gray-200 transition dark:text-white dark:bg-white/10 dark:hover:bg-white/20'>
+      <section className='bg-gray-100 flex max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[25rem] sm:group-even:pl-8 hover:bg-gray-200 transition dark:text-white dark:bg-white/10 dark:hover:bg-white/20'>
         <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]'>
           <h3 className='text-2xl font-semibold'>{title}</h3>
           <p className='mt-2 leading-relaxed text-gray-700 dark:text-white/70'>
@@ -38,7 +38,7 @@ export default function Project({ title, description, tags, imageUrl }:
             ))}
           </ul>
         </div>
-        <Image className='absolute hidden sm:block top-8 -right-40 w-[28.25rem]
+        <Image className='absolute hidden sm:block top-8 -right-40 w-[30rem] h-full
           rounded-t-lg shadow-2xl transition
           group-odd::right-[initial]
           group-even:-left-40
