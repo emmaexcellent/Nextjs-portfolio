45dbd112861a7a59393400d3aeefc454fea861d6
Emmanuel Excellent <68669639+Daddypastor@users.noreply.github.com>
2023-10-02 00:32:21 +0100

    Update Intro.tsx

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -89,8 +89,8 @@ export default function Intro() {
           focus:scale-110 hover:scale-110 active:scale-105
           transition cursor-pointer border
           borderBlack dark:bg-white/10'
-          href="/CV.pdf" download>
-          Download CV{" "}
+          href="/Resume.pdf" download>
+          Check Resume{" "}
           <HiDownload className="opacity-60
           group-hover:translate-y-1 transition"/>
         </a>
